20b5548b45b3dec87dd46eff8f14e3f24c5ac62d
NjilaBoris <njilaboris2@gmail.com>
2026-04-18 02:54:36 +0100

    First

---
+++ projects.js
@@ -0,0 +1,82 @@
+export const projectsData = [
+  {
+    name: "Nganu",
+    director: "Kang Quintus",
+    location: "Cameroon Douala",
+  },
+  {
+    name: "Rejected",
+    director: "Marcus Reynolds",
+    location: "Cameroon Douala",
+  },
+  {
+    name: "Mabanda",
+    director: "Kang Quintus",
+    location: "Cameroon, Douala",
+  },
+  {
+    name: "Ekie",
+    director: "Kang Quintus",
+    location: "Cameroon, Douala",
+  },
+  {
+    name: "Viewfinder",
+    director: "Kang Quintus",
+    location: "Cameroon, Douala",
+  },
+  {
+    name: "Rhythm Collective",
+    director: "Kang Quintus",
+    location: "Cameroon, Douala",
+  },
+  {
+    name: "Urban Odyssey",
+    director: "Kang Quintus",
+    location: "Cameroon, Douala",
+  },
+  {
+    name: "Prism No. 1",
+    director: "Kang Quintus",
+    location: "Private Estate - Sedona",
+  },
+  {
+    name: "Vision Quest",
+    director: "Kang Quintus",
+    location: "Elevation - Denver",
+  },
+  {
+    name: "Wavelength",
+    director: "Kai Nakamura",
+    location: "San Francisco, CA",
+  },
+  {
+    name: "Desert Horizon",
+    director: "Olivia",
+    location: "New Mexico",
+  },
+  {
+    name: "Spectrum",
+    director: "Ellis // Moss",
+    location: "Harmony Studio - Montreal",
+  },
+  {
+    name: "Vision Quest II",
+    director: "Hudson // Wright",
+    location: "Elevation Studios - Denver",
+  },
+  {
+    name: "Auteur",
+    director: "Leo Thompson",
+    location: "Berlin, DE",
+  },
+  {
+    name: "Retaliation",
+    director: "Kang Quintus",
+    location: "Cameroon, Douala",
+  },
+  {
+    name: "Fisherman Diary",
+    director: "Kang Quintus",
+    location: "Cameroon, Douala",
+  },
+];
